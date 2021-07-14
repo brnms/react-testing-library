60a7f3193a833d06e0e35ebf69d64c50263f3af4
Bruno Santos <bruno.matasantos@gmail.com>
2021-07-14 10:38:08 -0300

    Add files via upload

---
+++ src/components/Todo/__test__/Todo.test.js
@@ -0,0 +1,56 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import Todo from '../Todo';
+
+const MockTodo= () => {
+    return (
+        <BrowserRouter>
+            <Todo />
+        </BrowserRouter>
+    )
+}
+
+const addTask = (tasks) => {
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    const buttonElement = screen.getByRole("button", { name: /Add/i})
+    tasks.forEach(task => {
+        fireEvent.change(inputElement, { target: { value: task} });
+        fireEvent.click(buttonElement);
+    })
+    
+}
+
+describe("AddInput", () => {
+  it('should render the same text passed into title prop', () => {
+    render(<MockTodo />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    const buttonElement = screen.getByRole("button", { name: /Add/i})
+    fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping"} });
+    fireEvent.click(buttonElement);
+    const divElement = screen.getByText(/Go Grocery Shopping/i)
+    expect(divElement).toBeInTheDocument();
+  }); 
+
+  it('should render multiple items', () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping", "Pet my Cat", "Wash my Hands"])
+    const divElements = screen.getAllByTestId("task-container")
+    expect(divElements.length).toBe(3);
+  }); 
+
+  it('task should not have completed class when initially rendered', () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping"]);
+    const divElement = screen.getByText(/Go Grocery Shopping/i)
+    expect(divElement).not.toHaveClass("todo-item-active");
+  }); 
+
+  it('task should have completed class when clicked', () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping"]);
+    const divElement = screen.getByText(/Go Grocery Shopping/i)
+    fireEvent.click(divElement);
+    expect(divElement).toHaveClass("todo-item-active");
+  }); 
+
+});
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -11,67 +11,72 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
         </BrowserRouter>   
     )
 }
+describe("TodoFooter", () => {
+  
 
-it('should render the correct amount of incomplete tasks', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={5} />
-  );
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
+  it('should render the correct amount of incomplete tasks', () => {
+    render(
+        <MockTodoFooter numberOfIncompleteTasks={5} />
+    );
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(
+        <MockTodoFooter numberOfIncompleteTasks={1} />
+    );
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeTruthy();
-});
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeVisible();
-});
+// it('should render "task" when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter numberOfIncompleteTasks={1} />
+//   );
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toBeTruthy();
+// });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toContainHTML("p");
-});
+// it('should render "task" when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter numberOfIncompleteTasks={1} />
+//   );
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toBeVisible();
+// });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByTestId("para");
-  expect(paragraphElement).toHaveTextContent("1 task left");
-});
+// it('should render "task" when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter numberOfIncompleteTasks={1} />
+//   );
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toContainHTML("p");
+// });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByTestId("para");
-  expect(paragraphElement).not.toBeFalsy();
-});
+// it('should render "task" when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter numberOfIncompleteTasks={1} />
+//   );
+//   const paragraphElement = screen.getByTestId("para");
+//   expect(paragraphElement).toHaveTextContent("1 task left");
+// });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(
-      <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByTestId("para");
-  expect(paragraphElement.textContent).toBe("1 task left");
-});
+// it('should render "task" when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter numberOfIncompleteTasks={1} />
+//   );
+//   const paragraphElement = screen.getByTestId("para");
+//   expect(paragraphElement).not.toBeFalsy();
+// });
+
+// it('should render "task" when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter numberOfIncompleteTasks={1} />
+//   );
+//   const paragraphElement = screen.getByTestId("para");
+//   expect(paragraphElement.textContent).toBe("1 task left");
+// });
